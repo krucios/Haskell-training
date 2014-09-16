b68c2c831494c6384c2cac85ed86e85c5c885365
Andrei Pozniak <krucios@mail.ru>
2014-09-16 15:50:32 +0300

    Added some new functions

--- Facultative/HomeWork_1.hs
+++ Facultative/HomeWork_1.hs
@@ -2,6 +2,7 @@
 head' :: [a] -> a
 head' x = x !! 0
 
+
 -- Returns given list without first element
 tail' :: [a] -> [a]
 tail' x = drop 1 x
---
+++ Listings/Lists.hs
@@ -0,0 +1,25 @@
+factorial :: Integer -> Integer
+factorial x = if x > 1
+			  then x * factorial (x - 1)
+			  else 1
+			  
+circumference :: Float -> Float
+circumference x = 2 * pi * x
+
+circumference' :: Double -> Double
+circumference' x = 2 * pi * x
+
+max' :: (Ord a) => a -> a -> a -> a
+max' x y z = if x > y
+			then
+				if x > z
+				then
+					x
+				else
+					z
+			else
+				if y > z
+				then
+					y
+				else
+					z
